406628353a3f77efe10fb35d6ea0af1a4368b8eb
joseph1261997 <joseph.al@hotmail.it>
2024-04-05 13:08:53 +0200

    fix: aggiornati dati tabella da tornare e indirizzamento ad articles-by-position

--- src/components/TableArticles.tsx
+++ src/components/TableArticles.tsx
@@ -1,6 +1,6 @@
-import { useSelector } from "react-redux";
-import { selectProductsData, selectInfoSearch } from "../redux/productsSlice";
-import { TableArticlesProps } from "../utilities/types";
+import { useDispatch, useSelector } from "react-redux";
+import { selectProductsData, selectInfoSearch, setInfoSearch } from "../redux/productsSlice";
+// import { TableArticlesProps } from "../utilities/types";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,30 +8,57 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Link from "@mui/material/Link";
+import { useNavigate } from "react-router-dom";
 
-
-const TableArticles = ({ handleClick }: TableArticlesProps) => {
+const TableArticles: React.FC<{ isClickable: boolean }> = ({ isClickable }) => {
 
     const data = useSelector(selectProductsData);
     const infoSearch = useSelector(selectInfoSearch);
     const startIdx = 0;
     const endIdx = Math.min(infoSearch.RowForPage, data.length);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handlePositionClick = (position: string) => {
+        dispatch(setInfoSearch({ ...infoSearch, IndexPage: 0 }));
+        navigate(`/articles-by-position/${position}`, {
+            state: {
+                position: position
+            }
+        });
+    };
 
     return (
         <TableContainer component={Paper} sx={{ minHeight: 0 }}>
             <Table>
                 <TableHead>
                     <TableRow sx={{ backgroundColor: '#f0f0f0' }}>
+                        <TableCell><strong>Posizione</strong></TableCell>
+                        <TableCell><strong>Riferimento</strong></TableCell>
                         <TableCell><strong>Codice</strong></TableCell>
-                        <TableCell><strong>Prezzo</strong></TableCell>
+                        <TableCell><strong>Descrizione</strong></TableCell>
+                        <TableCell><strong>Listino</strong></TableCell>
+                        <TableCell><strong>Netto</strong></TableCell>
                         <TableCell><strong>Esistenza</strong></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {data.slice(startIdx, endIdx).map((product) => (
-                        <TableRow key={product.Codice} hover onClick={() => handleClick(product)}>
+                        <TableRow key={product.Codice} hover >
+                            <TableCell>
+                                {isClickable ?
+                                    <Link underline="hover" component='button' color='black' onClick={() => handlePositionClick(product.Posizione)} >{product.Posizione}</Link>
+                                    :
+                                    product.Posizione
+                                }
+
+                            </TableCell>
+                            <TableCell>{product.Riferimento}</TableCell>
                             <TableCell>{product.Codice}</TableCell>
-                            <TableCell>€ {product.Prezzo}</TableCell>
+                            <TableCell>{product.Descrizione.trim()}</TableCell>
+                            <TableCell>€ {product.Listino}</TableCell>
+                            <TableCell>€ {product.Netto}</TableCell>
                             <TableCell>{product.Esistenza}</TableCell>
                         </TableRow>
                     ))}
