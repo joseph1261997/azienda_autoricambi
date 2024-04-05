d8538ff5a30e51b71936c9eaf0bb13238fdb95c0
joseph1261997 <joseph.al@hotmail.it>
2024-04-05 13:06:16 +0200

    delete comment

--- src/utilities/api.tsx
+++ src/utilities/api.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL: 'https://192.168.10.222:15038',
     baseURL: 'https://progettogiuseppe.local:15038',
     withCredentials: true
 });
